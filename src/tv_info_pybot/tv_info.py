930c5ef2ff2ef3e895b77470558102ef0a98c0b5
Gary Sentosa <gary.sentosa@gmail.com>
2020-08-13 20:30:20 +0900

    Extract program schedule

--- src/tv_info_pybot/tv_info.py
+++ src/tv_info_pybot/tv_info.py
@@ -1,3 +1,4 @@
+import datetime
 import re
 
 
@@ -7,3 +8,27 @@ class TvInfoExtractor:
 
     def _extract_channel_name(self, raw_description):
         return re.search(r'\[(.+)\]', raw_description).group(1)
+
+    def _extract_program_schedule(self, raw_date, raw_description):
+        end_time = re.search(
+            r'[0-9]+\/[0-9]+\s[0-9]+:[0-9]+～([0-9]+):([0-9]+)',
+            raw_description
+        )
+        start_datetime = (
+            datetime
+            .datetime
+            .strptime(raw_date, "%Y-%m-%dT%H:%M%z")
+            .replace(tzinfo=None)
+        )
+        end_datetime = datetime.datetime(
+            start_datetime.year,
+            start_datetime.month,
+            start_datetime.day,
+            int(end_time.group(1)),
+            int(end_time.group(2))
+        )
+
+        return {
+            'start': start_datetime,
+            'end': end_datetime
+        }
--- tests/test_tv_info_extractor.py
+++ tests/test_tv_info_extractor.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+
 def test_extract_program_title(tv_info_extractor):
     raw_title = (
         'ネタパレ【爆笑新作!ミキ・四千頭身・'
@@ -16,5 +19,20 @@ def test_extract_channel_name(tv_info_extractor):
     raw_description = '8/14 23:40～0:10 [フジテレビ(Ch.8)]'
     expected = 'フジテレビ(Ch.8)'
     output = tv_info_extractor._extract_channel_name(raw_description)
+
+    assert output == expected
+
+
+def test_extract_program_schedule(tv_info_extractor):
+    raw_description = '8/14 23:40～0:10 [フジテレビ(Ch.8)]'
+    raw_date = '2020-08-14T23:40+09:00'
+    expected = {
+        'start': datetime(2020, 8, 14, 23, 40),
+        'end': datetime(2020, 8, 14, 0, 10)
+    }
+    output = tv_info_extractor._extract_program_schedule(
+        raw_date,
+        raw_description,
+    )
 
     assert output == expected
